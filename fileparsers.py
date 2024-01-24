0cf37cd9a014bfcd21c1d4a37b1239ac6ca9b847
MichaelBreau <breaum1@DESKTOP-MLS65FD>
2024-01-24 12:40:30 -0500

    Completed exceptions parser

--- fileparsers.py
+++ fileparsers.py
@@ -7,9 +7,13 @@ def parse_exceptions(path):
     exceptions_dictionary = {}
     with open(path, 'r') as file:
         for line in file:
+            line = line.rstrip()
             pals = line.split("|")
-            exceptions_dictionary[{pals[0], pals[1]}] = pals[2]
+            parents = [pals[0], pals[1]]
+            parents.sort()
+            parent_string = parents[0] + " " + parents[1]
+            exceptions_dictionary[parent_string] = pals[2]
     
-    print(exceptions_dictionary[{"Incineram", "Maraith"}])
+    return exceptions_dictionary
 
 parse_exceptions('exceptions.txt')
